8950128165f1b8fdbf342509ef2944da3abc465a
Tund229 <tundegounon@gmail.com>
2023-09-29 17:44:15 +0100

    banner fini

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -2,43 +2,42 @@ import { motion } from "framer-motion";
 const Banner = () => {
   return (
     <section
-      className="max-w-contentContainer mx-auto py-10 mdl:py-24 flex flex-col gap-4 lgl:gap-8 mdl:px-10 xl:px-4"
       id="home"
+      className="max-w-contentContainer mx-auto py-10 mdl:py-24 flex flex-col gap-4 lgl:gap-8 mdl:px-10 xl:px-12"
     >
-      <motion.h3
-        initial={{ y: 10, opacity: 0 }}
-        animate={{ y: 0, opacity: 1 }}
-        transition={{ duration: 0.5, delay: 0.6 }}
-        className="text-lg font-titleFont tracking-wide text-textGreen"
-      >
-        Hello , je m'appelle
-      </motion.h3>
       <motion.h1
         initial={{ y: 10, opacity: 0 }}
         animate={{ y: 0, opacity: 1 }}
         transition={{ duration: 0.5, delay: 0.7 }}
-        className="text-4xl lg:text-6xl font-titleFont font-semibold flex flex-col"
+        className="text-4xl lgl:text-4xl font-titleFont font-semibold flex flex-col"
       >
-        Tundé GOUNON{" "}
-        <span className="text-textDark mt-2 lgl:mt-4">Un passionné</span>
+        Tundé GOUNON,
+        <span className="text-textDark mt-2 lgl:mt-4">
+          Un passioné du code💻
+        </span>
       </motion.h1>
       <motion.p
         initial={{ y: 10, opacity: 0 }}
         animate={{ y: 0, opacity: 1 }}
         transition={{ duration: 0.5, delay: 0.8 }}
-        className="text-base md:max-w-[665px] text-textDark font-medium"
+        className="text-base md:max-w-[650px] text-textDark font-medium"
       >
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos, cum. Lorem
-        ipsum dolor sit amet consectetur adipisicing elit. Nam, incidunt!
+        Je suis un développeur web et mobile avec plus d'un an d'expérience dans
+        le domaine de la programmation. Bienvenue sur mon portfolio, où je
+        partage ma passion pour la création d'applications web et mobiles.
+        Explorez mon travail et n'hésitez pas à me contacter pour discuter de
+        collaborations potentielles.
       </motion.p>
-      <motion.button
-        initial={{ y: 10, opacity: 0 }}
-        animate={{ y: 0, opacity: 1 }}
-        transition={{ duration: 0.5, delay: 0.9 }}
-        className=" w-52 h-14 rounded-md text-textGreen text-sm border tracking-wide border-textGreen hover:bg-hoverColor duration-300"
-      >
-        Check out my Projets
-      </motion.button>
+      <a href="https://github.com/noorjsdivs" target="_blank">
+        <motion.button
+          initial={{ y: 10, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          transition={{ duration: 0.5, delay: 0.9 }}
+          className="w-52 h-14 text-sm font-titleFont border border-textGreen rounded-md text-textGreen tracking-wide hover:bg-hoverColor duration-300"
+        >
+          Mes projets
+        </motion.button>
+      </a>
     </section>
   );
 };
